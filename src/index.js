88691b8f9ed95d2476f9cdc10da31d5add7fb009
jluis <jluisvazquez.jv@gmail.com>
2022-01-24 17:55:30 -0600

    Usando la API de internacionalizacion

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@
 
 const baseUrl = "https://platzi-avo.vercel.app";
 
-const appNode = document.querySelector('app');
+const appNode = document.querySelector('#app');
+
+const formatPrice = (price) => {
+
+    const newPrice = new window.Intl.NumberFormat('en-En', {
+        style: "currency",
+        currency: "USD",
+    }).format(price)
+
+    return newPrice;
+};
 
 //web api
 // conectarnos al server
@@ -20,20 +30,33 @@ window
         responseJson.data.forEach((item) => {
             //crear imagen
             const imagen = document.createElement('img');
+            imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
             imagen.src = `${baseUrl}${item.image}`;
 
             //craer titulo
             const title = document.createElement('h2');
+            title.className = "text-lg";
             title.textContent = item.name;
 
             //craer precio
             const price = document.createElement('div');
-            price.textContent = item.price;
+            price.className = "text-gray-600";
+            price.textContent = formatPrice(item.price);
 
-            const container = document.createElement('div')
-            container.append(imagen, title, price);
+            //unir price y title
+            const priceAndTitle = document.createElement('div');
+            priceAndTitle.className = "text-center md:text-left";
+            priceAndTitle.appendChild(title);
+            priceAndTitle.appendChild(price);
 
-            todosLosItems.push(container)
+            //unir imagen con priceAndTitle 
+            const card = document.createElement('div');
+            card.className = "md:flex bg-white rounded-lg p-6 hover:big-gray-300";
+            card.appendChild(imagen);
+            card.appendChild(priceAndTitle);
+
+            todosLosItems.push(card);
         });
         appNode.append(...todosLosItems);
+        appNode.className = "mt-10 grid grid-cols-2 gap-2";
     });
